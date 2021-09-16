bac1cd65dab4d349a0317f4387fa659ac70a76c7
Maxime <maxime@vaugam.com>
2021-09-16 18:28:34 +0200

    fix(polygon): fix tokenUri on polygon

--- contracts/ArianeeSmartContract/ERC721Tradable.sol
+++ contracts/ArianeeSmartContract/ERC721Tradable.sol
@@ -2,7 +2,6 @@
 
 pragma solidity ^0.8.0;
 
-import "openzeppelin-solidity/contracts/token/ERC721/ERC721.sol";
 import "openzeppelin-solidity/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "openzeppelin-solidity/contracts/access/Ownable.sol";
 import "openzeppelin-solidity/contracts/utils/math/SafeMath.sol";
@@ -26,22 +25,42 @@ contract ProxyRegistry {
  */
 abstract contract ERC721Tradable is ContextMixin, ERC721Enumerable, NativeMetaTransaction, Ownable, Pausable {
 
-    address proxyRegistryAddress;
+    using Strings for uint256;
+    
+    mapping(address => bool) approvedOperator;
+    
+    /**
+   * @dev Base URI
+   */
+  string internal URIBase;
+
+  /**
+   * @dev Mapping from token id to URI.
+   */
+  mapping(uint256 => string) internal idToUri;
+  
+  /**
+   * @dev This emits when the uri base is udpated.
+   */
+  event SetNewUriBase(string _newUriBase);
+  
+  string constant NOT_VALID_CERT = "007003";
 
     constructor(
         string memory _name,
-        string memory _symbol,
-        address _proxyRegistryAddress
+        string memory _symbol
     ) ERC721(_name, _symbol) {
-        proxyRegistryAddress = _proxyRegistryAddress;
         _initializeEIP712(_name);
+        setUriBase("https://cert.arianee.org/");
     }
 
 
-    function baseTokenURI() virtual public view returns (string memory);
-
-    function tokenURI(uint256 _tokenId) override public view returns (string memory) {
-        return string(abi.encodePacked(baseTokenURI(), Strings.toString(_tokenId)));
+    function addApprovedOperator(address _newOperator) public onlyOwner(){
+        approvedOperator[_newOperator] = true;
+    }
+    
+    function removeApprovedOperator(address _newOperator) public onlyOwner(){
+        approvedOperator[_newOperator] = false;
     }
 
     /**
@@ -53,15 +72,12 @@ abstract contract ERC721Tradable is ContextMixin, ERC721Enumerable, NativeMetaTr
         view
         returns (bool)
     {
-        // Whitelist OpenSea proxy contract for easy trading.
-        ProxyRegistry proxyRegistry = ProxyRegistry(proxyRegistryAddress);
-        if (address(proxyRegistry.proxies(owner)) == operator) {
+        if (approvedOperator[operator]) {
             return true;
         }
 
         return super.isApprovedForAll(owner, operator);
     }
-    
 
 
     /**
@@ -75,4 +91,40 @@ abstract contract ERC721Tradable is ContextMixin, ERC721Enumerable, NativeMetaTr
     {
         return ContextMixin.msgSender();
     }
+    
+ 
+  /**
+   * @dev Base URI for computing {tokenURI}. If set, the resulting URI for each
+   * token will be the concatenation of the `baseURI` and the `tokenId`. Empty
+   * by default, can be overriden in child contracts.
+   */
+  function baseTokenURI() public view returns (string memory) {
+      return URIBase;
+  }
+  
+  /**
+   * @notice return the URI of a NFT.
+   * @param _tokenId uint256 ID of the NFT.
+   * @return URI of the NFT.
+   */
+  function tokenURI(uint256 _tokenId) override public view returns (string memory){
+      require(_exists(_tokenId), NOT_VALID_CERT);
+      if(bytes(idToUri[_tokenId]).length > 0){
+        return idToUri[_tokenId];
+      }
+      else{
+          return bytes(URIBase).length > 0 ? string(abi.encodePacked(URIBase, _tokenId.toString())) : "";
+      }
+  }
+  
+  
+  /**
+   * @notice Change the base URI address.
+   * @param _newURIBase the new URI base address.
+   */
+  function setUriBase(string memory _newURIBase) public onlyOwner(){
+      URIBase = _newURIBase;
+
+      emit SetNewUriBase(URIBase);
+  }
 }
